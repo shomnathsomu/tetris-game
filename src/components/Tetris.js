a4e0214cd5a2d9750526e684bbec305614401245
LinKin-22 <shomnathcse22@gmail.com>
2019-08-25 20:50:41 +0600

    added createStage function

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { createStage } from '../gameHelpers';
+
 //components
 import Stage from './Stage';
 import Display from './Display';
@@ -9,7 +11,7 @@ const Tetris = () => {
 
     return (
         <div>
-            <Stage />
+            <Stage stage = {createStage()}/>
             <aside>
                 <div>
                     <Display text="Score" />
